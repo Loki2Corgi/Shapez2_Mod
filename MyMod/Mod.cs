33902bb5cfba939068a9a52ccb06acc2153d0907
Loki2Corgi <152791789+Loki2Corgi@users.noreply.github.com>
2026-05-21 23:37:46 +0200

    Add files via upload

---
+++ MyMod/Mod.cs
@@ -0,0 +1,16 @@
+﻿using Game.Core.Modding;
+using ILogger = Core.Logging.ILogger;
+
+public class ExampleMod : IMod
+{
+    // Konstruktor: Wird in der Phase "Instantiated" aufgerufen
+    public ExampleMod(ILogger logger)
+    {
+        logger.Info?.Log("Hello, shapez 2");
+    }
+
+    // Wird beim Entladen/Beenden aufgerufen (Shutdown)
+    public void Dispose()
+    {
+    }
+}
